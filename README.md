3075a1f4cb2ba99b3ac9f329fb370404ec2e4310
Ron Gilson <rgilson@t1v.com>
2025-02-09 22:17:46 -0500

    demo commit

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
+## Purpose:
 
-Purpose:
 - build a "key tweak indexer" that computes and stores silent payments key tweaks
+  - user provides a sender public key and receiver sp address
+  - key tweak, txn output public key is computed
+  - store sender public key, txn output public key, tweak in db
 - building a server that can respond with key tweaks for a given blockhash
+  - webservice that responds key tweaks given a blockhash
 
+### *** Experimental - use at your own risk ***
 
+## Resources:
 
-Resources:
-https://github.com/bitcoin/bips/blob/master/bip-0352.mediawiki
-https://github.com/bitcoin/bitcoin/issues/28536
-https://delvingbitcoin.org/t/silent-payments-light-client-protocol/891/1
+* [BIP352](https://github.com/bitcoin/bips/blob/master/bip-0352.mediawiki)
+* [BIP352 Tracker](https://github.com/bitcoin/bitcoin/issues/28536)
+* [Main Website](https://silentpayments.xyz/)
+* [How Silent Payments Work](https://bitcoin.design/guide/how-it-works/silent-payments/)
+* [Block Filters](https://en.bitcoin.it/wiki/BIP_0157)
+* [Developer Podcast](https://podcasts.apple.com/us/podcast/silent-payments-a-bitcoin-username-with-josibake/id1415720320?i=1000656901291)
+* https://medium.com/@ottosch/how-silent-payments-work-41bea907d6b0
+* https://delvingbitcoin.org/t/silent-payments-light-client-protocol/891/1
 
-Light Client Filters
-https://podcasts.apple.com/us/podcast/silent-payments-a-bitcoin-username-with-josibake/id1415720320?i=1000656901291
+## Implementations:
 
-Implementations:
-https://github.com/bitcoin/bips/blob/master/bip-0352/reference.py
-https://github.com/cygnet3/rust-silentpayments
-https://github.com/cygnet3/sp-client
+Python:
+
+* https://github.com/bitcoin/bips/blob/master/bip-0352/reference.py
+
+Rust:
+
+* https://github.com/cygnet3/rust-silentpayments
+* https://github.com/cygnet3/sp-client
+
+Wallets:
+
+* https://silentpayments.xyz/docs/wallets/
+
--- tweak-indexer/src/main.rs
+++ tweak-indexer/src/main.rs
@@ -1,5 +1,6 @@
 use secp256k1::{rand, PublicKey, SecretKey, Scalar, Secp256k1};
 use sha2::{Sha256, Digest};
+use silentpayments::{SilentPaymentAddress, Network};
 
 fn compute_silent_payment_key_tweak(sender_pubkey: &PublicKey, receiver_scan_pubkey: &PublicKey) -> Scalar {
     let mut hasher = Sha256::new();
@@ -10,20 +11,39 @@ fn compute_silent_payment_key_tweak(sender_pubkey: &PublicKey, receiver_scan_pub
     Scalar::from_be_bytes(tweak_bytes.try_into().expect("32 bytes required")).expect("Tweak convert failed")
 }
 
+fn compute_spend_payment_address(tweak: Scalar, receiver_scan_pubkey: &PublicKey) -> PublicKey {
+    let secp = Secp256k1::new();
+    let tweak_point = PublicKey::from_secret_key(&secp, &SecretKey::from_slice(&tweak.to_be_bytes()).expect("Could not generate key from point"));
+    receiver_scan_pubkey.combine(&tweak_point).expect("Failed to combine keys")
+}
+
 fn main() {
     let secp = Secp256k1::new();
+
+    //Generate receiver scan key pair
+    let scan_privkey = SecretKey::new(&mut rand::thread_rng());
+    let scan_pubkey = PublicKey::from_secret_key(&secp, &scan_privkey);
+
+    //Generate receiver spend key pair
+    let spend_privkey = SecretKey::new(&mut rand::thread_rng());
+    let spend_pubkey = PublicKey::from_secret_key(&secp, &spend_privkey);
+
+    //Generate silent payment address
+    let silent_address = SilentPaymentAddress::new(scan_pubkey, spend_pubkey, Network::Mainnet, 0).unwrap();
+    println!("Silent Payment Address (Hex): {}", silent_address.to_string());
 
     //Generate sender key pair
     let sender_privkey = SecretKey::new(&mut rand::thread_rng());
     let sender_pubkey = PublicKey::from_secret_key(&secp, &sender_privkey);
     
-    //Generate receiver scan key pair
-    let receiver_privkey = SecretKey::new(&mut rand::thread_rng());
-    let receiver_pubkey = PublicKey::from_secret_key(&secp, &receiver_privkey);
-
-    let tweak = compute_silent_payment_key_tweak(&sender_pubkey, &receiver_pubkey);
-
+    let tweak = compute_silent_payment_key_tweak(&sender_pubkey, &silent_address.get_scan_key());
     let tweak_hex = hex::encode(tweak.to_be_bytes());
     println!("Tweak (Hex): {}", tweak_hex);
+
+    let txn_out_pubkey = compute_spend_payment_address(tweak, &silent_address.get_scan_key());
+    println!("Txn Out Pub Key (Hex): {}", hex::encode(txn_out_pubkey.serialize()));
+
 }
 
+//https://github.com/tokio-rs/axum
+//https://github.com/rusqlite/rusqlite
